ea006999c36dd598163d04cdd24c9b0f65fe11b9
xwz <wangze.cs@gmail.com>
2024-10-30 10:56:47 +0800

    comment out 'run_colmap'

--- utils/colmap2poses.py
+++ utils/colmap2poses.py
@@ -279,10 +279,11 @@ def gen_poses(basedir, match_type, factors=None):
     else:
         files_had = []
     if not all([f in files_had for f in files_needed]):
-        print( 'Need to run COLMAP' )
-        run_colmap(basedir, match_type)
-    else:
-        print('Don\'t need to run COLMAP')
+        raise ValueError('Need to run COLMAP first')
+        # print( 'Need to run COLMAP' )
+        # run_colmap(basedir, match_type)
+    # else:
+    #     print('Don\'t need to run COLMAP')
         
     print( 'Post-colmap')
     
@@ -580,60 +581,60 @@ import subprocess
 #     --output_path $DATASET_PATH/sparse
 
 # $ mkdir $DATASET_PATH/dense
-def run_colmap(basedir, match_type):
+# def run_colmap(basedir, match_type):
     
-    logfile_name = os.path.join(basedir, 'colmap_output.txt')
-    logfile = open(logfile_name, 'w')
+#     logfile_name = os.path.join(basedir, 'colmap_output.txt')
+#     logfile = open(logfile_name, 'w')
     
-    feature_extractor_args = [
-        '/home/jasonzh2/.local/bin/colmap', 'feature_extractor', 
-            '--database_path', os.path.join(basedir, 'database.db'), 
-            '--image_path', os.path.join(basedir, 'images'),
-            '--ImageReader.single_camera', '1',
-            # '--SiftExtraction.use_gpu', '0',
-    ]
-    feat_output = ( subprocess.check_output(feature_extractor_args, universal_newlines=True) )
-    logfile.write(feat_output)
-    print('Features extracted')
+#     feature_extractor_args = [
+#         '/home/jasonzh2/.local/bin/colmap', 'feature_extractor', 
+#             '--database_path', os.path.join(basedir, 'database.db'), 
+#             '--image_path', os.path.join(basedir, 'images'),
+#             '--ImageReader.single_camera', '1',
+#             # '--SiftExtraction.use_gpu', '0',
+#     ]
+#     feat_output = ( subprocess.check_output(feature_extractor_args, universal_newlines=True) )
+#     logfile.write(feat_output)
+#     print('Features extracted')
 
-    exhaustive_matcher_args = [
-        '/home/jasonzh2/.local/bin/colmap', match_type, 
-            '--database_path', os.path.join(basedir, 'database.db'), 
-    ]
+#     exhaustive_matcher_args = [
+#         '/home/jasonzh2/.local/bin/colmap', match_type, 
+#             '--database_path', os.path.join(basedir, 'database.db'), 
+#     ]
 
-    match_output = ( subprocess.check_output(exhaustive_matcher_args, universal_newlines=True) )
-    logfile.write(match_output)
-    print('Features matched')
+#     match_output = ( subprocess.check_output(exhaustive_matcher_args, universal_newlines=True) )
+#     logfile.write(match_output)
+#     print('Features matched')
     
-    p = os.path.join(basedir, 'sparse')
-    if not os.path.exists(p):
-        os.makedirs(p)
+#     p = os.path.join(basedir, 'sparse')
+#     if not os.path.exists(p):
+#         os.makedirs(p)
 
-    # mapper_args = [
-    #     'colmap', 'mapper', 
-    #         '--database_path', os.path.join(basedir, 'database.db'), 
-    #         '--image_path', os.path.join(basedir, 'images'),
-    #         '--output_path', os.path.join(basedir, 'sparse'),
-    #         '--Mapper.num_threads', '16',
-    #         '--Mapper.init_min_tri_angle', '4',
-    # ]
-    mapper_args = [
-        '/home/jasonzh2/.local/bin/colmap', 'mapper',
-            '--database_path', os.path.join(basedir, 'database.db'),
-            '--image_path', os.path.join(basedir, 'images'),
-            '--output_path', os.path.join(basedir, 'sparse'), # --export_path changed to --output_path in colmap 3.6
-            '--Mapper.num_threads', '16',
-            '--Mapper.init_min_tri_angle', '4',
-            '--Mapper.multiple_models', '0',
-            '--Mapper.extract_colors', '0',
-    ]
+#     # mapper_args = [
+#     #     'colmap', 'mapper', 
+#     #         '--database_path', os.path.join(basedir, 'database.db'), 
+#     #         '--image_path', os.path.join(basedir, 'images'),
+#     #         '--output_path', os.path.join(basedir, 'sparse'),
+#     #         '--Mapper.num_threads', '16',
+#     #         '--Mapper.init_min_tri_angle', '4',
+#     # ]
+#     mapper_args = [
+#         '/home/jasonzh2/.local/bin/colmap', 'mapper',
+#             '--database_path', os.path.join(basedir, 'database.db'),
+#             '--image_path', os.path.join(basedir, 'images'),
+#             '--output_path', os.path.join(basedir, 'sparse'), # --export_path changed to --output_path in colmap 3.6
+#             '--Mapper.num_threads', '16',
+#             '--Mapper.init_min_tri_angle', '4',
+#             '--Mapper.multiple_models', '0',
+#             '--Mapper.extract_colors', '0',
+#     ]
 
-    map_output = ( subprocess.check_output(mapper_args, universal_newlines=True) )
-    logfile.write(map_output)
-    logfile.close()
-    print('Sparse map created')
+#     map_output = ( subprocess.check_output(mapper_args, universal_newlines=True) )
+#     logfile.write(map_output)
+#     logfile.close()
+#     print('Sparse map created')
     
-    print( 'Finished running COLMAP, see {} for logs'.format(logfile_name) )
+#     print( 'Finished running COLMAP, see {} for logs'.format(logfile_name) )
 
 def main():
     if len(sys.argv) != 3:
